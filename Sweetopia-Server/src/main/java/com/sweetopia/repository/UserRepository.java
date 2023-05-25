4b9cc71754a2438c28128b0a1461510677ce682b
mdzahidhaider <mdzahidhaider@gmail.com>
2023-05-25 18:00:42 +0530

    update

--- Sweetopia-Server/src/main/java/com/sweetopia/repository/UserRepository.java
+++ Sweetopia-Server/src/main/java/com/sweetopia/repository/UserRepository.java
@@ -1,15 +1,14 @@
 
+package com.sweetopia.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import com.sweetopia.entity.User;
 
-
-//
-
 public interface UserRepository extends JpaRepository<User, Long> {
     
-    User findByUsername(String username);
+//    User findByUsername(String username);
     
 }
 
